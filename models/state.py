eb7ac0fe259c4d7a3c25058c8e7410ff61653aaa
Isaiah Kipyego <32485790+IsaiahKipyego@users.noreply.github.com>
2023-11-23 00:24:13 +0300

    state

---
+++ models/state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""State Module"""
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", cascade="all, delete", backref='state')
+    else:
+        @property
+        def cities(self):
+            """returns the list of City instances with state_id equals
+            to the current State.id"""
+            from models import storage
+            from models.city import City
+
+            cities = storage.all(City)
+            keys = cities.keys()
+            temp = []
+            for key in keys:
+                if cities[key].state_id == self.id:
+                    temp.append(cities[key])
+            return temp
